4dc20d6475ff805fe98fa76dd4a69efb1e57f12d
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-07-01 17:49:42 +0200

    form test try

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -1,4 +1,86 @@
-const form = document.querySelector(".feedback-form");
-const emailInput = document.getElementById("email");
-const messageTextarea = document.getElementById("message");
 
+function saveToLocalStorage(key, value) {
+    const jsonData = JSON.stringify(value);
+    localStorage.setItem(key, jsonData);
+  }
+  
+  
+  function loadFromLocalStorage(key) {
+    const json = localStorage.getItem(key);
+  
+    try {
+      const data = JSON.parse(json);
+      return data;
+    } catch {
+      return json;
+    }
+  }
+  
+  const formData = {
+    email: '',
+    message: '',
+  };
+  
+  const feedbackFormEl = document.querySelector('.feedback-form');
+  const feedbackFormState = 'feedback-form-state';
+  
+  feedbackFormEl.addEventListener('submit', event => {
+    event.preventDefault();
+  
+    let usermail = feedbackFormEl.elements.email.value;
+    let usermessage = feedbackFormEl.elements.message.value;
+  
+    usermail = usermail.trim();
+    usermessage = usermessage.trim();
+  
+    if (usermail === '' || usermessage === '') {
+      
+      alert('Fill please all fields');
+  
+    } else {
+      
+      console.log(formData);
+  
+      
+      localStorage.removeItem(feedbackFormState);
+  
+      formData.email = '';
+      formData.message = '';
+  
+      feedbackFormEl.reset();
+    }
+  });
+  
+  feedbackFormEl.addEventListener('input', event => {
+    
+    let usermail = feedbackFormEl.elements.email.value;
+    let usermessage = feedbackFormEl.elements.message.value;
+  
+    usermail = usermail.trim();
+    usermessage = usermessage.trim();
+  
+   
+    formData.email = usermail;
+    formData.message = usermessage;
+  
+    
+    saveToLocalStorage(feedbackFormState, formData);
+  });
+  
+  
+  const dataFromLS = localStorage.getItem(feedbackFormState);
+  
+  if (dataFromLS !== null) {
+    
+    const parseData = JSON.parse(dataFromLS);
+  
+    feedbackFormEl.elements.email.value = parseData.email;
+    feedbackFormEl.elements.message.value = parseData.message;
+  
+    formData.email = parseData.email;
+    formData.message = parseData.message;
+  }
+  // Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
